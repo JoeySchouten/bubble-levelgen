f52c029d56b6dbc17848d50eccddb4ea31fa3561
JoeySchouten <joey.schouten@gmail.com>
2024-06-26 11:08:02 +0200

    Fill generate_level skeleton

--- levelgen.py
+++ levelgen.py
@@ -39,7 +39,7 @@ FILLS = [   # These are based on default field width and height
 ]
 
 def _weighted_roll(elements_set):
-    """Return a random item with a weighted chance"""
+    """Return a random item with a weighted chance""" #thank you, Lemon ^^
     
     # Get the total chance
     total = sum([element.chance for element in elements_set])
@@ -58,27 +58,53 @@ def _weighted_roll(elements_set):
         else:
             chance_so_far += element.chance
             
-def _apply_element(element):
-    pass
+def _apply_element(element, list, config = GeneratorConfig()):
+    list_to_return = []
+    return list_to_return
 
-def _apply_fill(fill):
-    pass
+def _apply_fill(fill, list, config = GeneratorConfig()):
+    list_to_return = []
+    return list_to_return
 
-def _list_to_array(list):
-    pass
+def _list_to_2D(list):
+    return list
 
 def _list_to_string(list):
-    pass
+    return list
 
-def generate_level(config, elements_set, fill_set, return_string=False):
-    # get difficulty to spend
-    # make bubble list
-    # grab max row -> determine how manyth bubble from config
-    # pick fill and save it -> deduct cost
-    # grab design elements
-    # apply fill
-    # if return_string:
-        # give output as one string
-    # if not:
-        # give output as 2d array
-    pass
+def generate_level(world, level, stars, config=GeneratorConfig(), elements_set=DESIGN_ELEMENTS, fill_set=FILLS, return_string=False):
+    """Generates a 2d Array or string that contains all of the integers for the level .JSON-file 
+    based on entered config parameters, element and fill arrays."""
+
+    # Get total difficulty to spend on this level.
+    level_difficulty = (config.base_difficulty * world) + (config.diff_per_level * level) + (config.diff_per_star * stars)
+    spent_difficulty = 0
+
+    # Make bubble list, the field has a base width at even height numbers, and width-1 at odd numbers.
+    bubble_list = []
+    for height in range(0, config.field_height):
+        if height % 2 == 0:
+            bubble_list += ([0] * config.field_width)
+        else:
+            bubble_list += ([0] * (config.field_width-1))
+
+    # Pick a random fill, save it for later, and add the cost to what we have spent.
+    selected_fill = _weighted_roll(fill_set)
+    #spent_difficulty += selected_fill.cost
+
+    # Roll design elements, apply them, and add the cost to what we have spent.
+    while spent_difficulty < level_difficulty:
+        selected_element = _weighted_roll(elements_set)
+        #spent_difficulty += selected_element.cost
+        bubble_list = _apply_element(selected_element, bubble_list)
+        break
+
+    # Apply the fill to our level
+    bubble_list = _apply_fill(selected_fill, bubble_list)
+
+    if return_string: # Give output as one string.
+        return _list_to_string(bubble_list)
+    else: # Give output as 2D List.
+        return _list_to_2D(bubble_list)
+    
+print(generate_level(1,1,1))
